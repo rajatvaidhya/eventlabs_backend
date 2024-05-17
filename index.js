c645b6407d2bbdc427c8ed458e798616c0b0dc4e
rajatvaidhya <83221992+rajatvaidhya@users.noreply.github.com>
2024-05-17 15:56:55 +0530

    getYourEvents issue resolved

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin:["https://eventlabs-frontend.vercel.app"],
+    origin:["https://eventlabs-frontend.vercel.app","http://localhost:3000"],
     methods:["POST","GET","PUT","DELETE"],
     credentials:true
   }
